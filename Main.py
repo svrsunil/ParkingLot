942fcba080235b8f191e3fcd1c7bc9207c4909b3
svrsunil <svrsunil@gmail.com>
2019-06-30 00:15:24 +0800

    status method is created to return list of car details.
    Modified var name in ParkingLot init method to match with the input text

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 29 22:56:26 2019
+Created on Sat Jun 29 16:54:16 2019
 
 @author: svrsunil
 """
@@ -8,10 +8,10 @@ import sys
 
 class ParkingLot:
     
-    def __init__(self, regNo, color,lot):
-        self.regNo = regNo
-        self.color = color
-        self.lot = lot
+    def __init__(self, registration_number, cars_with_colour,slot_number):
+        self.registration_number = registration_number
+        self.cars_with_colour = cars_with_colour
+        self.slot_number = slot_number
         
 avalLot = []
 totLot = 0
@@ -19,6 +19,8 @@ alcoDict = {}
 
 def setLot(usrIp):
     global totLot
+    if(totLot > 0) :
+        return 'Slot already set'
     totLot =  int(usrIp.split(' ')[1])
     global avalLot
     avalLot = [(i+1) for i in range(totalLot)]
@@ -39,9 +41,11 @@ def carPark(usrIp):
     return msg
 
 def carLeave(usrIp):
+    print(usrIp)
     msg = 'Slot number is Invalid'
     i = 0
     arr = usrIp.split(' ')
+    print(arr)
     key = int(arr[1])
     if key in alcoDict:
         del alcoDict[key]
@@ -50,21 +54,46 @@ def carLeave(usrIp):
         
     return msg
 
+def carInquiry(usrIp):
+    
+    qtns,ans = usrIp.split(' ')
+    qtn = qtns.split('_for_')
+    print(qtn)
+    result = ''
+    for key in sorted(alcoDict):
+        result = result + ', ' + str(getattr(alcoDict[key], qtn[0]))
+        
+    return result[2:]
+    
+def status():
+    result = 'Slot No.    Registration No    Colour'
+     
+    for key in sorted(alcoDict):
+        parkingLot = alcoDict[key]
+        result = result + '\n'+str(key)+'           '+parkingLot.registration_number+'      '+parkingLot.cars_with_colour
+    
+    return result
+ 
 def interact(usrIp):
     usrIp = input(usrIp)
     
     rsp = ''
     if('create_parking_lot' in usrIp):
+        
         rsp= setLot(usrIp)
     elif('park' in usrIp) :
         rsp= carPark(usrIp)
-    elif('park' in usrIp) :
-        rsp= carPark(usrIp)
+    elif('leave' in usrIp) :
+        rsp= carLeave(usrIp)
+    elif('status' in usrIp) :
+        rsp= status()        
+    elif('_for_' in usrIp) :
+        rsp= carInquiry(usrIp)        
     elif('exit' == usrIp) :
         sys.exit(0)
     else :
         rsp = 'Invalid Arugment'
-    
+    print(rsp)
     interact('')
 
 if __name__== "__main__":
@@ -72,4 +101,5 @@ if __name__== "__main__":
     if len(sys.argv) > 1:
         print(sys.argv[1])
     else :
+        print("No")
         interact('')
